628e2026b0c3b0152981bb7fc97ea0662ff360ac
Aditya <chauhan.adie@gmail.com>
2018-05-16 10:15:32 -0600

    Java_syntaxrules

---
+++ Party.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+public class Party{
+    public static void main(String[] args) {
+        System.out.println("Hello World");    
+        //java syntax - 
+        /*
+        *Each statement must end with semicolon;
+        *A single line statement begins with two forward slashes //
+        *Most white spaces does not matter
+        *variable are declared with a name and a type
+        *classes and methods must be defined within a pair of curly braces
+        */
+    }
+}
