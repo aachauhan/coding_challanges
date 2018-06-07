ada88ca3248901be01ecec8f97eb3447c9cdebff
Aditya <chauhan.adie@gmail.com>
2018-06-07 16:00:44 -0600

    refined prime factor function requirements

--- Palindrome.java
+++ Palindrome.java
@@ -6,6 +6,10 @@ public class Palindrome {
 		//from the problem I know that I need sollowing components
 		//function to find factors especially prime factors
 		//find the prime factor of big number 600851475143
+		//the prime factor function takes a number as an arguement
+		//this number is checked(either with loop or recursively) for its prime factors
+		//a prime factor is a number that divides other number completely and also is a prime number
+		//e.g. if a%b = 0, and b is also a prime number then b is a prime factor of a
     }
     public void isPalindrome(String n){
         String original, reverse = "";
